9aec1a04534a2019497b2a2bf450a3b65e13512b
Tanth811 <tanth811@gmail.com>
2023-10-20 00:44:51 +0700

    modified:   app/models/db.js

--- app/models/db.js
+++ app/models/db.js
@@ -1,12 +1,15 @@
 const mysql = require("mysql2");
-const dbConfig = require("../config/db.config.js");
-// Create a connection to the database
-const connection = mysql.createConnection({
-host: dbConfig.HOST,
-user: dbConfig.USER,
-password: dbConfig.PASSWORD,
-database: dbConfig.DB
-});
+// db.js
+require("dotenv").config()
+const connection = mysql.createConnection(process.env.DATABASE_URL)
+// const dbConfig = require("../config/db.config.js");
+// // Create a connection to the database
+// const connection = mysql.createConnection({
+// host: dbConfig.HOST,
+// user: dbConfig.USER,
+// password: dbConfig.PASSWORD,
+// database: dbConfig.DB
+// });
 // open the MySQL connection
 connection.connect(error => {
 if (error) console.log("MySQL connection: " + error);
